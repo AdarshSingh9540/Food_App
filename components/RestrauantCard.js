887df9b608b53e756971999b6a081b7936527960
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-01-28 13:07:34 +0530

    destructuring

--- components/RestrauantCard.js
+++ components/RestrauantCard.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { IMG_CDN_URL } from '../Constant'; // Import IMG_CDN_URL here
 
 const RestrauantCard = (props) => {
-  const { name, cuisines, costForTwo, avgRating, areaName, cloudinaryImageId } = props.restaurant; // Include cloudinaryImageId
+  
 
   return (
     <div className="card">
       {/* <img src={IMG_CDN_URL + cloudinaryImageId} alt="img" /> Use cloudinaryImageId here */}
-      <h2>{name}</h2>
-      <h3>{cuisines}</h3>
-      <h4>{costForTwo}</h4>
+      <h2>{props.restaurant.info.name}</h2>
+      <h3>{props.restaurant.info.name}</h3>
+      <h4>{props.restaurant.info.name}</h4>
       <h4>
-        {avgRating} * {/* Assuming avgRating is a number */}
+        {props.restaurant.info.name} * {/* Assuming avgRating is a number */}
       </h4>
-      <h4>{areaName}</h4>
+      <h4>{props.restaurant.info.name}</h4>
     </div>
   );
 };
