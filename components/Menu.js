ef7b3093ff2fda6c0cd5bc409d211c558f23b645
Adarsh <adarsh9540984202@gmail.com>
2024-02-08 22:18:14 +0530

    Change in Menu component

--- components/Menu.js
+++ components/Menu.js
@@ -16,8 +16,8 @@ const Menu = () =>{
         <div className="Menu">
             <div>
           <h1>Restaurant id: {resId} </h1>
-          <h2>Restaurant name: {res.cards[2].card.card.info.name}</h2>
-          <img src={IMG_CDN_URL+res.cards[2].card.card.info.cloudinaryImageId} />
+          <h2 >Restaurant name: {res.cards[2].card.card.info.name}</h2>
+          <img style={{width:"500px"}} src={IMG_CDN_URL+res.cards[2].card.card.info.cloudinaryImageId} />
           <h3>{res.cards[2].card.card.info.areaName}</h3>
           <h3>{res.cards[2].card.card.info.city}</h3>
           <h3>{res.cards[2].card.card.info.avgRating} stars</h3>
@@ -26,6 +26,7 @@ const Menu = () =>{
         <div>
         <h1>Menu</h1>
         <ul>
+        {console.log(res.cards[5].groupedCard.cardGroupMap.REGULAR.cards)}
     {res.cards[5].groupedCard.cardGroupMap.REGULAR.cards.map((groupedCard) => (
       groupedCard?.card?.card?.itemCards?.map((itemCard) => (
         <li key={itemCard?.card?.info?.id}>{itemCard?.card?.info?.name}</li>
--- components/RestrauantCard.js
+++ components/RestrauantCard.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 import { IMG_CDN_URL } from '../Constant'; // Import IMG_CDN_URL here
+import Shimmer from './Shimmer';
+
 
 const RestrauantCard = (props) => {
-  return (
+  const isOpen = props.restaurant.info.isOpen;
+  const [isOpenState, setIsOpenState] = useState(isOpen);
+
+  if (isOpenState === true) {
+    setIsOpenState("Open");
+  }
+  if(isOpenState===false){
+    setIsOpenState("close");
+  }
+  
+  return !props? <Shimmer/>: ( 
+    <div >
+    <div className="details">
+      <h1 className="isOpen" style={{background:"green", color:"white", padding:"5px",margin:"2px", fontSize:"20px", borderRadius:"8px"}}>{isOpenState.toString()}</h1>
+      </div>
     <div className="card">
       <img  src={IMG_CDN_URL + props.restaurant.info.cloudinaryImageId} alt="img" /> 
       <h1>{props.restaurant.info.name}</h1>
@@ -12,6 +28,7 @@ const RestrauantCard = (props) => {
         {props.restaurant.info.cuisines.join(", ")} {/* Assuming avgRating is a number */}
       </h4>
       <h4>{props.restaurant.info.costForTwo}</h4>
+    </div>
     </div>
   );
 };
